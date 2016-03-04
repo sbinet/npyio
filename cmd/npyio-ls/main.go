ecbe793c473895b69dcadbda421babcf01a889ad
Sebastien Binet <binet@cern.ch>
2016-03-05 00:15:15 +0100

    npyio-ls: detect and handle .npz archives

--- cmd/npyio-ls/main.go
+++ cmd/npyio-ls/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"reflect"
 	"strings"
 
 	"github.com/gonum/matrix/mat64"
 	"github.com/sbinet/npyio"
+)
+
+var (
+	zipMagic = [4]byte{'P', 'K', 3, 4}
 )
 
 func main() {
@@ -25,51 +31,106 @@ func main() {
 		if i > 0 {
 			fmt.Printf("\n")
 		}
+		fmt.Printf(strings.Repeat("=", 80) + "\n")
+		fmt.Printf("file: %v\n", fname)
+
 		f, err := os.Open(fname)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("error: %v\n", err)
+			allgood = false
 		}
 		defer f.Close()
 
-		r, err := npyio.NewReader(f)
+		// detect .npz files (check if we find a ZIP file magic header)
+		var hdr [6]byte
+		_, err = f.Read(hdr[:])
 		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf(strings.Repeat("=", 80) + "\n")
-		fmt.Printf("file: %v\n", fname)
-		fmt.Printf("npy-header: %v\n", r.Header)
-
-		if len(r.Header.Descr.Shape) > 2 {
-			rt := npyio.TypeFrom(r.Header.Descr.Type)
-			if rt == nil {
-				fmt.Printf("error: no reflect.Type for %q\n", r.Header.Descr.Type)
-				allgood = false
-				continue
-			}
-			rv := reflect.New(reflect.SliceOf(rt))
-			err = r.Read(rv.Interface())
-			if err != nil {
-				allgood = false
-				fmt.Printf("error: %v\n", err)
-				continue
-			}
-			fmt.Printf("data = %v\n", rv.Elem().Interface())
-			continue
-		}
-
-		var m mat64.Dense
-		err = r.Read(&m)
-		if err != nil {
+			fmt.Printf("error sniffing file format: %v\n", err)
 			allgood = false
-			fmt.Printf("error: %v\n", err)
+			continue
+		}
+		_, err = f.Seek(0, 0)
+		if err != nil {
+			fmt.Printf("error rewinding file: %v\n", err)
+			allgood = false
 			continue
 		}
 
-		fmt.Printf("data = %v\n", mat64.Formatted(&m, mat64.Prefix("       ")))
+		switch {
+		case bytes.Equal(npyio.Magic[:], hdr[:]):
+			allgood = display(f, fname) && allgood
+		case bytes.Equal(zipMagic[:], hdr[:len(zipMagic)]):
+			fi, err := f.Stat()
+			if err != nil {
+				fmt.Printf("error stat-ing file: %v\n", err)
+				allgood = false
+				continue
+			}
+			zr, err := zip.NewReader(f, fi.Size())
+			if err != nil {
+				fmt.Printf("error creating zip-reader: %v\n", err)
+				allgood = false
+				continue
+			}
+
+			for ii, f := range zr.File {
+				r, err := f.Open()
+				if err != nil {
+					fmt.Printf("error opening entry %s: %v\n", f.Name, err)
+					allgood = false
+					continue
+				}
+				defer r.Close()
+				if ii > 0 {
+					fmt.Printf("\n")
+				}
+				fmt.Printf("entry: %s\n", f.Name)
+				allgood = display(r, fname+"@"+f.Name) && allgood
+			}
+		default:
+			fmt.Printf("error: unknown magic header %q\n", string(hdr[:]))
+			allgood = false
+			continue
+		}
 	}
 
 	if !allgood {
 		os.Exit(1)
 	}
+}
+
+func display(f io.Reader, fname string) bool {
+	r, err := npyio.NewReader(f)
+	if err != nil {
+		fmt.Printf("error creating reader %s: %v\n", fname, err)
+		return false
+	}
+
+	fmt.Printf("npy-header: %v\n", r.Header)
+
+	if len(r.Header.Descr.Shape) > 2 {
+		rt := npyio.TypeFrom(r.Header.Descr.Type)
+		if rt == nil {
+			fmt.Printf("error: no reflect.Type for %q\n", r.Header.Descr.Type)
+			return false
+		}
+		rv := reflect.New(reflect.SliceOf(rt))
+		err = r.Read(rv.Interface())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			return false
+		}
+		fmt.Printf("data = %v\n", rv.Elem().Interface())
+		return true
+	}
+
+	var m mat64.Dense
+	err = r.Read(&m)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return false
+	}
+
+	fmt.Printf("data = %v\n", mat64.Formatted(&m, mat64.Prefix("       ")))
+	return true
 }

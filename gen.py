371356b0a9cb48bac52add50b2cc73978d893938
Sebastien Binet <binet@cern.ch>
2016-03-04 10:52:23 +0100

    all: fix dtype of testdata

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 from __future__ import print_function
 import numpy as np
 
@@ -9,19 +10,19 @@ for dt in [
     for order in ["f", "c"]:
         with open("testdata/data_%s_2x3_%sorder.npy" % (dt, order), "w") as f:
             print(">>> %s" % f.name)
-            arr = np.arange(6).reshape(2, 3, order=order)
+            arr = np.arange(6, dtype=dt).reshape(2, 3, order=order)
             np.save(f, arr)
             pass
         
         with open("testdata/data_%s_6x1_%sorder.npy" % (dt, order), "w") as f:
             print(">>> %s" % f.name)
-            arr = np.arange(6).reshape(6,1, order=order)
+            arr = np.arange(6, dtype=dt).reshape(6,1, order=order)
             np.save(f, arr)
             pass
 
         with open("testdata/data_%s_1x1_%sorder.npy" % (dt,order), "w") as f:
             print(">>> %s" % f.name)
-            arr = np.arange(1).reshape(1,1, order=order)
+            arr = np.arange(1, dtype=dt).reshape(1,1, order=order)
             arr[0] = 42
             np.save(f, arr)
             pass

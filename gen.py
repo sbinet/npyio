965130afa1d07bb687d0f04cbb5c9fe49a6b872f
Sebastien Binet <binet@cern.ch>
2016-03-08 20:52:42 +0100

    all: add LICENSE header

--- gen.py
+++ gen.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python2
+
+# Copyright 2016 The npyio Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from __future__ import print_function
 import numpy as np
 
--- npyio.go
+++ npyio.go
@@ -1,3 +1,7 @@
+// Copyright 2016 The npyio Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package npyio provides read/write access to files following the NumPy data file format:
 //  http://docs.scipy.org/doc/numpy-1.10.1/neps/npy-format.html
 //

c6baa5a576fcd090970f502ea6c1235637f01a36
Sebastien Binet <binet@cern.ch>
2016-03-13 11:39:16 +0100

    npyio: add bench for Read
    BenchmarkReadDense-4          	    2000	    591613 ns/op	   88969 B/op	    6033 allocs/op
    BenchmarkReadFloat32Slice-4   	    3000	    568131 ns/op	   60625 B/op	    6029 allocs/op
    BenchmarkReadFloat64Slice-4   	    3000	    586335 ns/op	   88817 B/op	    6029 allocs/op
    BenchmarkReadBoolSlice-4      	    5000	    378422 ns/op	   50481 B/op	    5029 allocs/op
    BenchmarkReadUintSlice-4      	    3000	    511918 ns/op	   88818 B/op	    6029 allocs/op
    BenchmarkReadUint8Slice-4     	    3000	    472299 ns/op	   50480 B/op	    6029 allocs/op
    BenchmarkReadUint16Slice-4    	    3000	    478089 ns/op	   52545 B/op	    6029 allocs/op
    BenchmarkReadUint32Slice-4    	    3000	    514241 ns/op	   64625 B/op	    6029 allocs/op
    BenchmarkReadUint64Slice-4    	    3000	    512523 ns/op	   88817 B/op	    6029 allocs/op
    BenchmarkReadIntSlice-4       	    3000	    519941 ns/op	   88817 B/op	    6029 allocs/op
    BenchmarkReadInt8Slice-4      	    3000	    464097 ns/op	   50481 B/op	    6029 allocs/op
    BenchmarkReadInt16Slice-4     	    3000	    480922 ns/op	   52544 B/op	    6029 allocs/op
    BenchmarkReadInt32Slice-4     	    3000	    505642 ns/op	   64625 B/op	    6029 allocs/op
    BenchmarkReadInt64Slice-4     	    3000	    519283 ns/op	   88817 B/op	    6029 allocs/op
    BenchmarkReadComplex64Slice-4 	    3000	    599218 ns/op	   88817 B/op	    6029 allocs/op
    BenchmarkReadComplex128Slice-4	    2000	    612020 ns/op	  145218 B/op	    6029 allocs/op
    BenchmarkReadIntArray-4       	    3000	    419153 ns/op	   48600 B/op	    5018 allocs/op
    BenchmarkReadFloat64Array-4   	    3000	    490748 ns/op	   48600 B/op	    5018 allocs/op

--- npyio_test.go
+++ npyio_test.go
@@ -5,6 +5,7 @@
 package npyio
 
 import (
+	"bytes"
 	"io/ioutil"
 	"testing"
 
@@ -189,5 +190,254 @@ func BenchmarkWriteFloat64Array(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		_ = Write(w, &data)
+	}
+}
+
+type creader struct {
+	buf []byte
+	pos int
+}
+
+func (r *creader) Read(data []byte) (int, error) {
+	n := copy(data, r.buf[r.pos:r.pos+len(data)])
+	r.pos += n
+	return n, nil
+}
+
+func (r *creader) reset() {
+	r.pos = 0
+}
+
+func BenchmarkReadDense(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, mat64.NewDense(100, 10, make([]float64, 1000)))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var m mat64.Dense
+		_ = Read(r, &m)
+		r.reset()
+	}
+}
+
+func BenchmarkReadFloat32Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]float32, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []float32
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadFloat64Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]float64, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []float64
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadBoolSlice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]bool, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []bool
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadUintSlice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]uint, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []uint
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadUint8Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]uint8, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []uint8
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadUint16Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]uint16, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []uint16
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadUint32Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]uint32, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []uint32
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadUint64Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]uint64, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []uint64
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadIntSlice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []int
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadInt8Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int8, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []int8
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadInt16Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int16, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []int16
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadInt32Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int32, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []int32
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadInt64Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int64, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []int64
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadComplex64Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]complex64, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []complex64
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadComplex128Slice(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]complex128, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data []complex128
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadIntArray(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]int, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data [1000]int
+		_ = Read(r, &data)
+		r.reset()
+	}
+}
+
+func BenchmarkReadFloat64Array(b *testing.B) {
+	buf := new(bytes.Buffer)
+	_ = Write(buf, make([]float64, 1000))
+	r := &creader{buf: buf.Bytes()}
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var data [1000]float64
+		_ = Read(r, &data)
+		r.reset()
 	}
 }
